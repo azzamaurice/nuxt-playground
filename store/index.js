32804dccdfdc7340b10ffda3162c9f28d02d7b65
azzamaurice <aaron.maurice@gmail.com>
2021-02-25 06:47:52 +1000

    Syntax tweak

--- store/index.js
+++ store/index.js
@@ -1,11 +1,11 @@
 export const actions = {
-    callAction({ dispatch }, { module, method, payload }) {
+    callAction: ({ dispatch }, { module, method, payload }) => {
         dispatch(`${module}/${method}`, payload, { root: true })
     }
 }
 
 export const getters = {
-    callGetter: (_state, _getters, _rootState, rootGetters) => ({ module, getter }) => {
-        return rootGetters[`${module}/${getter}`] || undefined
-    }
+    callGetter: (_state, _getters, _rootState, rootGetters) => ({ module, getter }) => (
+        rootGetters[`${module}/${getter}`] || undefined
+    )
 }
